adac2b04904668fb3d7d8c4e71dae7a29317ad0c
NamrataSalve <namratasalve1999@gmail.com>
2024-10-03 14:28:35 +0530

    [Namrata] Added: display welcome massage on main

---
+++ src/com/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.addressbooksystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to Address Book program");
+    }
+}
